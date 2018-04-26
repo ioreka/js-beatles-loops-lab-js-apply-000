0e2d12153d58463b170ee1db5a2d3733858f2d17
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-26 17:16:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ function theBeatlesPlay(musicians, instruments) {
   const musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
   const instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
 
-for
+for (letBeatlesIndex = 0; BeatlesIndex < )
+}
+
+
+let animals = ["Grizzly Bear", "Sloth", "Sea Lion"];
+
+for (let animalIndex = 0; animalIndex < animals.length; animalIndex++) {
+  console.log(animals[animalIndex]);
 }
