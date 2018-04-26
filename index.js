899b859c74d30cd4c737ba65bc28f6bbcc4e955e
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-26 17:13:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
+  const musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+  const instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
 
 }
