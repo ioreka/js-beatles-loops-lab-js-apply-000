3e0d54e7b7838594f0837ef9dc6d9448eb046bc3
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-26 17:28:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ for (letBeatlesIndex = 0; BeatlesIndex < musicians.length; BeatlesIndex++) {
   console.log(`${musicians} plays ${instruments}`)
 }
 }
+
+
+function theBeatlesPlay(musicians, instruments) {
++  var returnArr  = [];
++  for(let i = 0; i < musicians.length; i++){
++    returnArr.push(musicians[i] + " plays " + instruments[i])
++  }
++  console.log(returnArr)
++  return returnArr
++}
